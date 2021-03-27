9afd02fd72833b18a206948763d69c910465adaf
Levizera <levigames321@gmail.com>
2021-03-27 14:35:01 -0300

    Arquivos atualizados

--- bitcoin_realtime/price_tracker_formatting.py
+++
@@ -1,5 +0,0 @@
-def remove_special_char(old_string, to_remove):
-    new_string = old_string
-    for special_char in to_remove:
-        new_string = new_string.replace(special_char, '')
-    return new_string
